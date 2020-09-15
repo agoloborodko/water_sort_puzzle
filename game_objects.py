e39e3bd76005a4a5fb24d9abb57ece22ad981c52
a.goloborodko <isitreallyimportant7@yandex.ru>
2020-09-15 18:00:55 +0300

    Added more comprehensive check for vial init arguments

--- game_objects.py
+++ game_objects.py
@@ -4,7 +4,13 @@ from collections import UserList
 class Vial(UserList):
 
     def __init__(self, max_size, initlist=None):
-        if initlist is not None:
-            assert len(initlist) <= max_size
+        check_vial_arguments_meet_requirements(max_size, initlist)
+
         super().__init__(initlist)
         self.max_size = max_size
+
+
+def check_vial_arguments_meet_requirements(max_size, initlist):
+    assert max_size > 0
+    if initlist is not None:
+        assert len(initlist) <= max_size
--- tests.py
+++ tests.py
@@ -11,6 +11,14 @@ class TestVial(unittest.TestCase):
         self.assertEqual(vial, [])
         self.assertEqual(vial.max_size, 5)
 
+        vial = obj.Vial(4, [1, 2, 3])
+        self.assertEqual(vial, [1, 2, 3])
+
+        with self.assertRaises(AssertionError):
+            obj.Vial(2, [1, 2, 3])
+            obj.Vial(0, [1, 2, 3])
+            obj.Vial(0)
+
 
 if __name__ == '__main__':
     unittest.main()
