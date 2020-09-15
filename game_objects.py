c8e68c7f06b89e24e91132306e2b29801d593029
a.goloborodko <isitreallyimportant7@yandex.ru>
2020-09-15 15:59:37 +0300

    Added max_size attribute to Vial class

--- game_objects.py
+++ game_objects.py
@@ -3,5 +3,8 @@ from collections import UserList
 
 class Vial(UserList):
 
-    def __init__(self, initlist=None):
+    def __init__(self, max_size, initlist=None):
+        if initlist is not None:
+            assert len(initlist) <= max_size
         super().__init__(initlist)
+        self.max_size = max_size
--- tests.py
+++ tests.py
@@ -5,9 +5,11 @@ import game_objects as obj
 class TestVial(unittest.TestCase):
 
     def test_init(self):
-        vial = obj.Vial()
+        vial = obj.Vial(5)
         self.assertIsInstance(vial, obj.Vial)
         self.assertEqual(len(vial), 0)
+        self.assertEqual(vial, [])
+        self.assertEqual(vial.max_size, 5)
 
 
 if __name__ == '__main__':
