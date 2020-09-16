176b79a56cd674385e3a81cbdb70819ad1ca9575
agoloborodko <Call grey lama for help>
2020-09-17 00:16:02 +0300

    added new test my function cannot pass

--- puzzle_solver.py
+++ puzzle_solver.py
@@ -1,4 +1,5 @@
 from game_objects import VialBoard
+import copy
 
 
 def solve(vial_board):
@@ -8,5 +9,8 @@ def solve(vial_board):
         for i in range(len(vial_board)):
             for j in range(len(vial_board)):
                 if vial_board.can_move(i, j):
-                    vial_board.move(i, j)
-                    return solve(vial_board)
+                    new_vial_board = copy.deepcopy(vial_board)
+                    new_vial_board.move(i, j)
+                    solve(new_vial_board)
+
+
